80d984dcb0cecbbad0001cc85a891e09a94ad1e1
Kyle Bashour <kylebshr@me.com>
2020-05-22 02:22:30 -0700

    Remove print

--- Sources/Application/AppDelegate.swift
+++ Sources/Application/AppDelegate.swift
@@ -11,12 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        for family in UIFont.familyNames.sorted() {
-            let names = UIFont.fontNames(forFamilyName: family)
-            print("Family: \(family) Font names: \(names)")
-        }
-
         return true
     }
 }
